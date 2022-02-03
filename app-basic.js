9ce14cef0f015aab6c02ad4ef45cf78de198f2b1
kotbassdev <kotbass.dev@gmail.com>
2022-02-03 09:59:27 +0700

    update required and call myfirst module

--- app-basic.js
+++ app-basic.js
@@ -1,7 +1,10 @@
 var http = require('http')
+var dt = require('./modules/myfirst-module')
 
 http.createServer(function(req,res){
 	res.writeHead(200,{"Content-Type":"text/html"})
+	res.write(`The date and time: ${dt.myDateTime()}`)
+	res.write("<br />")
 	res.end("Hello NodeJs Basic")
 }).listen(5000,function(){
 	console.log("Server Start On Port:5000")
