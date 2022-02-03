7ab4605ff46de3115cba1f9262261ee457b90ca2
kotbassdev <kotbass.dev@gmail.com>
2022-02-03 11:32:30 +0700

    add module formidable

---
+++ app-uploadfile-basic.js
@@ -0,0 +1,6 @@
+var http = require('http')
+var formidable = require('formidable')
+
+http.createServer((req,res) => {
+
+}).listen(5002,() => console.log('start server on port:5002 for test upload file'))
